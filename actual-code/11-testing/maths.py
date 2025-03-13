938d5d33db99c7847e18cbc2e16726be2e900c51
Kevin Cunningham <kevin@kevincunningham.co.uk>
2025-03-13 08:29:55 +0000

    adds code

--- actual-code/11-testing/maths.py
+++ actual-code/11-testing/maths.py
@@ -1,3 +1,12 @@
 def add(a, b):
+    if not isinstance(a, (int, float)):
+        raise TypeError("Parameter must be a number (float or int).")
+
+    if not isinstance(b, (int, float)):
+        raise TypeError("Parameter must be a number (float or int).")
+
     return a + b
 
+
+def subtract(a, b):
+    return a - b
--- actual-code/11-testing/test_maths.py
+++ actual-code/11-testing/test_maths.py
@@ -1,4 +1,4 @@
-from maths import add
+from maths import add, subtract
 import pytest
 # Arrange - Given I have ...
 # Act     - When I do ...
@@ -24,6 +24,29 @@ def test_adding_two_numbers_produces_a_correct_result(number1, number2, expected
 
     # Assert  - Then something will be true
     assert expected_result == actual_result
+
+
+@pytest.mark.skip(reason="Don't know why it's failing")
+def test_something_i_have_not_finished_yet():
+    assert False
+
+
+def test_fails_if_adding_non_numbers():
+    with pytest.raises(TypeError):
+        add([], {})
+
+def test_fails_if_adding_two_strings():
+    with pytest.raises(TypeError):
+        add("Hello", "Goodbye")  # "Hello" + "Goodbye" = "HelloGoodbye"
+
+
+# Subtraction
+def test_can_correctly_subtract_two_numbers():
+    assert subtract(10, 7) == 3
+
+def test_can_correctly_subtract_two_decimals():
+    assert subtract(9.2, 1.2) == pytest.approx(8.)
+
 
 # Test Driven Development
 # -> Red - Green - Refactor
