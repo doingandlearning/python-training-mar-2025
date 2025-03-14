515e998374a037f2137076a72d50497d60d67afa
Kevin Cunningham <kevin@kevincunningham.co.uk>
2025-03-14 08:47:46 +0000

    Adds the pickle

--- actual-code/13-numpy-and-matplotlib/09-rehydrate.py
+++ actual-code/13-numpy-and-matplotlib/09-rehydrate.py
@@ -6,4 +6,10 @@ with open("saved_figure.pkl", "rb") as file:
     loaded_fig = pickle.load(file)
 
 print(loaded_fig)
-loaded_fig.show()  #
+
+# Apparently this works in some environments
+# loaded_fig.show()
+
+# This is what I had to do to show the figure again.
+plt.figure(loaded_fig.number)
+plt.show() 
